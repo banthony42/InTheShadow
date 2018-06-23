04131fa5390883bb77c52b13e12faf4219e0b2c2
Anthony BARA <banthony@e2r10p20.42.fr>
2018-06-23 19:39:59 +0200

    wip menu - graphics and aesthetic

---
+++ InTheShadows/Assets/Scripts/SpotMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotMovement : MonoBehaviour {
+
+    public float speed;
+    public float lim;
+
+    private Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+        targetPosition = new Vector3(Random.Range(-lim, lim), Random.Range(-lim, lim), transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+        if (Vector3.Distance(transform.position, targetPosition) <= 0.05f)
+            targetPosition = new Vector3(Random.Range(-lim, lim), Random.Range(-lim, lim), transform.position.z);
+   }
+}
