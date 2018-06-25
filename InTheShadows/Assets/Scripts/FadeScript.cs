26b6233d64bc13a028fb47b6543411a245b233ee
Anthony BARA <banthony@e2r9p14.42.fr>
2018-06-25 16:14:35 +0200

    wip menu, sound, music

---
+++ InTheShadows/Assets/Scripts/FadeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScript : MonoBehaviour {
+    public float fadeTime;
+
+    private float timer = 0f;
+    private UnityEngine.UI.RawImage rawImage;
+	// Use this for initialization
+	void Start () {
+        rawImage = GetComponent<UnityEngine.UI.RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rawImage.color = Color.Lerp(rawImage.color, Color.clear, timer);
+        if (timer < 1)
+        {
+            timer += Time.deltaTime / fadeTime;
+        }
+        if (rawImage.color == Color.clear)
+            gameObject.SetActive(false);
+	}
+}
---
+++ InTheShadows/Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonScript : MonoBehaviour {
+
+    public AudioClip buttonClic;
+    public AudioClip buttonTransition;
+
+    private string sceneToLoad = "none";
+    private AudioSource soundPlayer;
+
+    // Use this for initialization
+	void Start () {
+        soundPlayer = GetComponent<AudioSource>();
+        soundPlayer.volume = 0.1f;
+	}
+	
+    public void pointerEnter()
+    {
+        Debug.Log("test");
+        soundPlayer.PlayOneShot(buttonTransition);
+    }
+
+    public void onClickButton(string button)
+    {
+        soundPlayer.volume = 0.5f;
+        soundPlayer.clip = buttonClic;
+        soundPlayer.Play();
+        if (button == "classic" || button == "test")
+            sceneToLoad = "LevelSelect";
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (sceneToLoad != "none" && soundPlayer.time > 1.3f)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+            sceneToLoad = "none";
+        }
+	}
+}
