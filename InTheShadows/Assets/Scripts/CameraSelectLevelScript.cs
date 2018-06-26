1379ce65cf9f527fbfe36f26aa84cd3b0649e928
Anthony BARA <banthony@e1r2p17.42.fr>
2018-06-26 18:30:57 +0200

    wip select level scene

--- InTheShadows/Assets/Scripts/CameraSelectLevelScript.cs
+++ InTheShadows/Assets/Scripts/CameraSelectLevelScript.cs
@@ -7,29 +7,48 @@ public class CameraSelectLevelScript : MonoBehaviour
 
     public List<GameObject> level;
     public AudioClip levelSwhitchSound;
+    public AudioClip buttonClic;
+    public UnityEngine.UI.Text UI_levelName;
+    public GameObject fade;
 
     private AudioSource player;
     private Vector3 cameraDestination;
-    private Transform startPosition;    // pos(0,-45,15) rot(-45,0,0)
+    private Vector3 startPosition;    // pos(0,-45,15) rot(-45,0,0)
     private int indexLevel = -1;
     private float remaining;
     private float remainingLimit = 5f;
     private float speedTravel = 5f;
+    private int tmpIndex;
+    private Camera myCam;
+    private string sceneToLoad = "none";
 
     // Use this for initialization
     void Start()
     {
+        myCam = gameObject.GetComponent<Camera>();
         player = GetComponent<AudioSource>();
-        startPosition = transform;
+        startPosition = transform.position;
         cameraDestination = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (sceneToLoad != "none" && player.time > 1.3f)
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+
+        // Dezoom effect on the beginning of the scene
+        if (myCam.fieldOfView < 60)
+        {
+            if (myCam.fieldOfView < 1.1)
+                player.PlayOneShot(levelSwhitchSound);
+            myCam.fieldOfView += (myCam.fieldOfView * speedTravel) * Time.deltaTime;
+        }
+
         if (Input.GetKeyDown("escape"))
             UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
 
+        // Clic Handler on a level
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -39,27 +58,37 @@ public class CameraSelectLevelScript : MonoBehaviour
                 Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 1);
                 if (hit.collider.tag == "level")
                 {
-                    cameraDestination = hit.collider.transform.position;
-                    cameraDestination.y = hit.collider.transform.position.y - 10;
-                    cameraDestination.z = 40f;
+                    if ((tmpIndex = hit.collider.GetComponent<LevelScript>().levelIndex) != indexLevel)
+                    {
+                        indexLevel = tmpIndex;
+                        cameraDestination = hit.collider.transform.position;
+                        cameraDestination.y = hit.collider.transform.position.y - 10;
+                        cameraDestination.z = 40f;
+                        player.PlayOneShot(levelSwhitchSound);
+                    }
+                    else
+                    {
+                        player.clip = buttonClic;
+                        player.Play();
+                        fade.GetComponent<UnityEngine.UI.RawImage>().raycastTarget = true;
+                        fade.GetComponent<FadeScript>().target = Color.black;
+                        fade.GetComponent<FadeScript>().timer = 0f;
+                        sceneToLoad = "Menu";
+                    }
                 }
             }
-
         }
-
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f )
+        // Scroll to increment or decrement level view
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0f || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("back");
             if (indexLevel >= 0 && remaining <= remainingLimit)
             {
                 indexLevel--;
                 player.PlayOneShot(levelSwhitchSound);
             }
         }
-
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("front");
             if (indexLevel < level.Count - 1 && remaining <= remainingLimit)
             {
                 indexLevel++;
@@ -67,15 +96,21 @@ public class CameraSelectLevelScript : MonoBehaviour
             }
         }
 
+        // Calcul of the new position
         if (indexLevel >= 0)
         {
+            UI_levelName.text = level[indexLevel].GetComponent<LevelScript>().levelName;
             cameraDestination = level[indexLevel].transform.position;
             cameraDestination.y -= 10;
             cameraDestination.z = 40f;
         }
         else
-            cameraDestination = startPosition.position;
+        {
+            UI_levelName.text = "-";
+            cameraDestination = startPosition;
+        }
 
+        // Camera movement
         if (cameraDestination != Vector3.zero)
             transform.position = Vector3.Lerp(transform.position, cameraDestination, Time.deltaTime * speedTravel);
         remaining = Vector3.Distance(transform.position, cameraDestination);
---
+++ gitremoteShadow.sh
@@ -0,0 +1,2 @@
+git remote add originMyGit https://github.com/banthony42/InTheShadows.git
+git remote set-url --add --push origin https://github.com/banthony42/InTheShadows.git
