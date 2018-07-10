d9725199843bd4663b13bd34d221394581292ae6
Anthony BARA <banthony@e2r9p18.42.fr>
2018-07-10 16:48:25 +0200

    update room textures, beginning of gameplay

---
+++ InTheShadows/Assets/Scripts/ModelRotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRotation : MonoBehaviour
+{
+
+    public bool rotation_y;
+    public bool rotation_x;
+    public bool move;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            if (move && Input.GetKey(KeyCode.LeftControl))
+            {
+                transform.Translate(Input.GetAxis("Mouse X") * Time.deltaTime, Input.GetAxis("Mouse Y") * Time.deltaTime, 0);
+            }
+            else
+            {
+                if (rotation_x)
+                {
+                    transform.Rotate(Vector3.right * Input.GetAxis("Mouse Y") * Time.deltaTime * 100);
+                }
+                if (rotation_y)
+                {
+                    transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * 100);
+                }
+            }
+        }
+    }
+}
