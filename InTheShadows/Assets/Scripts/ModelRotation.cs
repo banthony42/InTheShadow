28c9ad6668a601a5753260ebdf88b73c7d9be23b
Anthony BARA <banthony@e2r9p18.42.fr>
2018-07-10 18:47:04 +0200

    wip victory check

--- InTheShadows/Assets/Scripts/ModelRotation.cs
+++ InTheShadows/Assets/Scripts/ModelRotation.cs
@@ -10,11 +10,11 @@ public class ModelRotation : MonoBehaviour
     public bool move;
     public float moveSpeed;
 
-    private UnityEngine.Quaternion rotationDest;
+    private Quaternion rotationDest = Quaternion.identity;
     private Vector3 positionDest;
 
     private Vector3 translateVec;
-    private UnityEngine.Quaternion rotateVec;
+    private Quaternion rotateVec = Quaternion.identity;
 
     // Use this for initialization
     void Start()
@@ -26,28 +26,23 @@ public class ModelRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetMouseButton(0))
         {
             if (move && Input.GetKey(KeyCode.LeftShift))
-            {
-                Debug.Log("Update translateVec");
                 positionDest = new Vector3(transform.position.x + Input.GetAxis("Mouse X") * moveSpeed, transform.position.y + Input.GetAxis("Mouse Y") * moveSpeed, transform.position.z);
-            }
             else
             {
                 if (rotation_x && Input.GetKey(KeyCode.LeftControl))
-                {
                     transform.Rotate(Vector3.right * Input.GetAxis("Mouse Y") * Time.deltaTime * 100);
-                }
-                if (rotation_y && !Input.GetKey(KeyCode.LeftControl))
-                {
+                else if (rotation_y)
                     transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * 100);
-                }
             }
         }
 
         if (move && Vector3.Distance(transform.position, positionDest) > 0.2f)
             transform.position = Vector3.Lerp(transform.position, positionDest, Time.deltaTime * moveSpeed);
+
+        if (transform.rotation.eulerAngles.Equals(Quaternion.Euler(0, 180, 0)))
+            Debug.Log("--WIN--");
     }
 }
