1d92383ff49107721755aca8f29062453fc7b5ea
Anthony BARA <banthony@e1r10p7.42.fr>
2018-07-14 13:13:25 +0200

    update git remote script

--- gitremoteShadow.sh
+++ gitremoteShadow.sh
@@ -1,2 +1,3 @@
-git remote add originMyGit https://github.com/banthony42/InTheShadows.git
+git remote add originIntra vogsphere@vogsphere.42.fr:intra/2018/activities/in_the_shadows/banthony
 git remote set-url --add --push origin https://github.com/banthony42/InTheShadows.git
+git remote set-url --add --push origin vogsphere@vogsphere.42.fr:intra/2018/activities/in_the_shadows/banthony
