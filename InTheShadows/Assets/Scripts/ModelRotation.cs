12281932fd7bbb9cb211fe62f99004f3c3b5e71b
Anthony BARA <banthony@e1r7p9.42.fr>
2018-07-14 22:39:45 +0200

    wip second level, work on smooth rotation

--- InTheShadows/Assets/Scripts/ModelRotation.cs
+++ InTheShadows/Assets/Scripts/ModelRotation.cs
@@ -20,6 +20,7 @@ public class ModelRotation : MonoBehaviour
         get { return _win; }
     }
 
+    private Quaternion rotationDest;
     private Vector3 positionDest;
     private float limit;
     private Vector3 initialPosition;
@@ -33,15 +34,34 @@ public class ModelRotation : MonoBehaviour
         initialPosition = transform.position;
         initialRotation = transform.rotation;
         positionDest = transform.position;
+        rotationDest = transform.rotation;
     }
 
     bool checkWin()
     {
-        float delta = Mathf.DeltaAngle(transform.rotation.eulerAngles.y, 180);
-        if (delta >=  (-1 * limit) && delta <= limit)
-            return true;
+        if (currentLevel == 0)
+        {
+            float delta = Mathf.DeltaAngle(transform.rotation.eulerAngles.y, 180);
+            if (delta >= (-1 * limit) && delta <= limit)
+                return true;
+        }
+        if (currentLevel == 1)
+        {
+        }
         return false;
     }
+
+    // Dont work
+    //bool deltaQuaternion(Quaternion delta, float lim)
+    //{
+    //    //if (delta.x < (-1 * lim) && delta.x > lim)
+    //        //return false;
+    //    if (delta.y >= (-1 * lim) && delta.y <= lim)
+    //        return false;
+    //    //else if (delta.z < (-1 * lim) && delta.z > lim)
+    //    //return false;
+    //    return true;
+    //}
 
     // Update is called once per frame
     void Update()
@@ -51,6 +71,7 @@ public class ModelRotation : MonoBehaviour
             transform.position = initialPosition;
             transform.rotation = initialRotation;
             positionDest = transform.position;
+            rotationDest = transform.rotation;
         }
         if (checkWin() && !_win)
         {
@@ -68,11 +89,19 @@ public class ModelRotation : MonoBehaviour
                 if (rotation_x && Input.GetKey(KeyCode.LeftControl))
                     transform.Rotate(Vector3.right * Input.GetAxis("Mouse Y") * Time.deltaTime * 100);
                 else if (rotation_y)
+                {
+                    //rotationDest = transform.rotation;
+                    //rotationDest.eulerAngles += new Vector3(0, Input.GetAxis("Mouse X") * 10, 0);
                     transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * 100);
+                }
             }
         }
 
         if (move && Vector3.Distance(transform.position, positionDest) > 0.2f)
             transform.position = Vector3.Lerp(transform.position, positionDest, Time.deltaTime * moveSpeed);
+
+        // Dont work
+        //if (deltaQuaternion(rotationDest * Quaternion.Inverse(transform.rotation), limit))
+            //transform.rotation = Quaternion.Lerp(transform.rotation, rotationDest, Time.deltaTime * moveSpeed);
     }
 }
