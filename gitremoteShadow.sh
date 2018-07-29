9c92bb304abb7ca35f0274bc593be7a97749a3d6
Anthony BARA <banthony@e3r6p17.42.fr>
2018-07-29 14:28:20 +0200

    delete gitremote script, add a build a playable version of the game

--- gitremoteShadow.sh
+++
@@ -1,3 +0,0 @@
-git remote add originIntra vogsphere@vogsphere.42.fr:intra/2018/activities/in_the_shadows/banthony
-git remote set-url --add --push origin https://github.com/banthony42/InTheShadows.git
-git remote set-url --add --push origin vogsphere@vogsphere.42.fr:intra/2018/activities/in_the_shadows/banthony
